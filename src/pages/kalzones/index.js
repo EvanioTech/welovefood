df03b0b5b5a9b3d2e8b7040ed67c53f792daa24d
Seu Nome <seu-email@example.com>
2024-12-30 17:18:52 -0300

    page kalzone finalizada

--- src/pages/kalzones/index.js
+++ src/pages/kalzones/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
 
 const Kalzones = () => {
+    const [hamburgues, setHamburgues] = useState([
+        {   id: 1,
+            name: 'Kalzone de Filé Mignon',
+            description: 'Molho de tomate, mussarela, filé mignon, cebola, azeitona, orégano e azeite.',
+            price: 'R$ 45,00',
+            img: 'https://admin.minikalzone.com.br/uploads/product/2/66aa857fac0183.29515226.png'},
+            {id: 2,
+                name: 'Kalzone de Frango',
+                description: 'Recheio cremoso de frango desfiado com catupiry, temperado com ervas e especiarias, envolto em uma massa dourada e macia.',
+               price: 35.00,
+               img: 'https://admin.minikalzone.com.br/uploads/product/13/66aa86d1d95a98.95992680.png',
+               },{id: 3,
+                name: 'Kalzone de Camarão',
+                description: 'Camarões suculentos em um molho especial com toque de alho e ervas, combinados com queijo cremoso, dentro de uma massa leve e crocante.',
+               price: 35.00,
+               img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6r_0yWztPSS7a8ywgun-iFJWk264mdlZCBQ&s',
+               },{id: 4,
+                name: 'Kalzone de Chocolate',
+                description: 'Uma explosão doce com recheio de chocolate derretido, finalizado com uma fina camada de açúcar polvilhado sobre a massa macia.',
+               price: 35.00,
+               img: 'https://www.minikalzone.com.br/static/image-2-e75b44e091b060ee2f663dbddd87be6d.png',
+               },
+    ]);
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Kalzones Component</Text>
+            <Text style={styles.textTitle}>Kalzones</Text>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={hamburgues}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => (
+                    <View style={styles.card}>
+                        <TouchableOpacity></TouchableOpacity>
+                        <Text style={styles.text}>{item.name}</Text>
+                        <Text style={styles.text}>R$ {item.price}</Text>
+                        <View style={styles.cardItem}>
+                        <Image source={{uri: item.img}} style={styles.img} />
+                        <Text style={styles.textDesc}>{item.description}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.btn} onPress={() => {alert('item adicionado!')}}>
+                            <Text style={styles.textbtn}>Adicionar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 };
@@ -15,11 +57,55 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
+        padding: 10,
+        
+    },
+    textTitle: {
+        fontSize: 40,
+        color: '#000',
+        marginTop: 50,
+        marginBottom: 20,
+    },
+    btn:{
+        backgroundColor: 'blue',
+        padding: 5,
+        borderRadius: 5,
+        marginTop: 10,
+        alignItems: 'center',
+        marginBottom: 15,
+        justifyContent: 'center',
+        width: '99%'
+    },
+    textbtn: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    img: {
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+        marginLeft: -20,
     },
     text: {
         fontSize: 20,
         color: '#000',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    cardItem: {
+        padding: 30,
+        margin: 5,
+        borderRadius: 10,
+        backgroundColor: '#f0f0f0',
+        flexDirection: 'row',
+        marginTop: 15,
+        
+    },
+    textDesc: {
+        fontSize: 19,
+        color: '#000',
+        textAlign: 'center',
+        width: '60%',
     },
 });
-
 export {Kalzones};
--- src/pages/sobremesas/index.js
+++ src/pages/sobremesas/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
 
 const Sobremesas = () => {
+    const [hamburgues, setHamburgues] = useState([
+        {   id: 1,
+            name: 'Pudim de Leite',
+           description: 'Pudim de leite condensado com calda de caramelo.',
+           price: 'R$ 15,00',
+           img: 'https://s2-receitas.glbimg.com/YTXTFuRbAtHmHJV0ddq9QJPqYt8=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2021/P/T/BRcriaQP6Lfn7NWtestA/pudim-de-leite-receita.jpg'
+       },
+            {id: 2,
+                name: 'Mousse de Maracujá',
+                description: 'Mousse leve e aerado de maracujá, com sabor tropical e uma delicada calda de sementes da fruta.',
+               price: 35.00,
+               img: 'https://static.itdg.com.br/images/640-440/8fed8f60d3c8e3990396e2478cbc7f2a/shutterstock-1905617575-1-.jpg',
+               },{id: 3,
+                name: 'Brownie de Chocolate',
+                description: 'Brownie macio e intenso, com textura densa e pedacinhos de chocolate, perfeito para amantes de doces.',
+               price: 35.00,
+               img: 'https://static.itdg.com.br/images/640-440/5d139b703a9df7604abf863a3bd76aa6/252863-shutterstock-1938293728.jpg',
+               },{id: 4,
+                name: 'Cheesecake de Morango',
+                description: 'Base de biscoito crocante com creme suave de cream cheese, coberto com uma generosa camada de calda de morango.',
+               price: 35.00,
+               img: 'https://www.minhareceita.com.br/app/uploads/2023/07/x-calabre-destaque-1.jpg',
+               },
+    ]);
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Sobremesas Component</Text>
+            <Text style={styles.textTitle}>Sobremesas</Text>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={hamburgues}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => (
+                    <View style={styles.card}>
+                        <TouchableOpacity></TouchableOpacity>
+                        <Text style={styles.text}>{item.name}</Text>
+                        <Text style={styles.text}>R$ {item.price}</Text>
+                        <View style={styles.cardItem}>
+                        <Image source={{uri: item.img}} style={styles.img} />
+                        <Text style={styles.textDesc}>{item.description}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.btn} onPress={() => {alert('item adicionado!')}}>
+                            <Text style={styles.textbtn}>Adicionar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 };
@@ -15,10 +58,55 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
+        padding: 10,
+        
+    },
+    textTitle: {
+        fontSize: 40,
+        color: '#000',
+        marginTop: 50,
+        marginBottom: 20,
+    },
+    btn:{
+        backgroundColor: 'blue',
+        padding: 5,
+        borderRadius: 5,
+        marginTop: 10,
+        alignItems: 'center',
+        marginBottom: 15,
+        justifyContent: 'center',
+        width: '99%'
+    },
+    textbtn: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    img: {
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+        marginLeft: -20,
     },
     text: {
         fontSize: 20,
         color: '#000',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    cardItem: {
+        padding: 30,
+        margin: 5,
+        borderRadius: 10,
+        backgroundColor: '#f0f0f0',
+        flexDirection: 'row',
+        marginTop: 15,
+        
+    },
+    textDesc: {
+        fontSize: 19,
+        color: '#000',
+        textAlign: 'center',
+        width: '60%',
     },
 });
 
